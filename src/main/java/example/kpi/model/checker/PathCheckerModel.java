abd2c5323c132bade454309fc910ed0299fbc40c
asem <a7chenko@gmail.com>
2021-11-15 22:50:06 +0200

    Small fixes #2

--- src/main/java/example/kpi/model/checker/PathCheckerModel.java
+++ src/main/java/example/kpi/model/checker/PathCheckerModel.java
@@ -3,8 +3,11 @@ package example.kpi.model.checker;
 import example.kpi.model.result.Issue;
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class PathCheckerModel {
+    private UUID id;
     private Issue issue;
     private PathCheckerType type;
     private String pattern;
